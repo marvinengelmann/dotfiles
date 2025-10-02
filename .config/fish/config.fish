a8f2de738c07e38dee4dd9f5d14311ce1305b93c
Marvin Engelmann <business@engelmann.technology>
2025-10-03 01:28:25 +0200

    ✨ feat(fish): Add nnn navigation function and set Helix as default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,18 @@ set -e fish_user_paths
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/opt/libpq/bin
-fish_add_path /Users/marvinengelmann/.local/bin
-fish_add_path /Users/marvinengelmann/.bun/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.bun/bin
+
+set -x EDITOR hx
 
 alias lg='lazygit'
+
+function n
+    set -l nnn_tmpfile ~/.config/nnn/.lastd
+    nnn -Hdeo $argv
+    if test -e $nnn_tmpfile
+        source $nnn_tmpfile
+        rm $nnn_tmpfile
+    end
+end
